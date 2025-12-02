c04e4657d7537487c976ae09a728a99599e3215f
Daniel Lechner <lechner.d@gmail.com>
2025-12-02 13:10:11 +0100

    feat: day one, part two

--- day-1/day-1.py
+++ day-1/day-1.py
@@ -1,4 +1,4 @@
-def solve_safe_dial(instructions):
+def solve_safe_dial(instructions, count_passes=False):
     position = 50
     zero_count = 0
 
@@ -6,13 +6,25 @@ def solve_safe_dial(instructions):
         direction = instruction[0]
         distance = int(instruction[1:])
 
-        if direction == 'L':
-            position = (position - distance) % 100
+        if count_passes:
+            if direction == 'R':
+                for i in range(1, distance + 1):
+                    if (position + i) % 100 == 0:
+                        zero_count += 1
+                position = (position + distance) % 100
+            else:
+                for i in range(1, distance + 1):
+                    if (position - i) % 100 == 0:
+                        zero_count += 1
+                position = (position - distance) % 100
         else:
-            position = (position + distance) % 100
+            if direction == 'L':
+                position = (position - distance) % 100
+            else:
+                position = (position + distance) % 100
 
-        if position == 0:
-            zero_count += 1
+            if position == 0:
+                zero_count += 1
 
     return zero_count
 
@@ -21,9 +33,12 @@ example = [
     'L55', 'L1', 'L99', 'R14', 'L82'
 ]
 
-print(f'Example result: {solve_safe_dial(example)}')
+print(f'Example Part 1: {solve_safe_dial(example)}')
+print(f'Example Part 2: {solve_safe_dial(example, count_passes=True)}')
 
 with open('input.txt', 'r') as f:
     instructions = [line.strip() for line in f.readlines()]
-    password = solve_safe_dial(instructions)
-    print(f'Password: {password}')
+    password1 = solve_safe_dial(instructions)
+    password2 = solve_safe_dial(instructions, count_passes=True)
+    print(f'Password Part 1: {password1}')
+    print(f'Password Part 2: {password2}')
